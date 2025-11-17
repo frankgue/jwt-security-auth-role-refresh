48ed82f282bc9900b45cd51367321d60991b962e
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-11-17 15:02:22 +0100

    Implementing JWTService

---
+++ src/main/java/com/gkfcsolution/jwtsecurityauthrolerefresh/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.gkfcsolution.jwtsecurityauthrolerefresh.dto;/**
+ * Created on 2025 at 12:20
+ * File: LoginRequest.java.java
+ * Project: jwt-security-auth-role-refresh
+ *
+ * @author Frank GUEKENG
+ * @date 17/11/2025
+ * @time 12:20
+ *
+ */
+public class LoginRequest {
+}
---
+++ src/main/java/com/gkfcsolution/jwtsecurityauthrolerefresh/dto/TokenPair.java
@@ -0,0 +1,12 @@
+package com.gkfcsolution.jwtsecurityauthrolerefresh.dto;/**
+ * Created on 2025 at 12:44
+ * File: TokenPair.java.java
+ * Project: jwt-security-auth-role-refresh
+ *
+ * @author Frank GUEKENG
+ * @date 17/11/2025
+ * @time 12:44
+ *
+ */
+public class TokenPair {
+}
